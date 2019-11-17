5efadae38c96b4125c06dfe7c89040b69fe71661
abdo <abdelsalem.yabrag@hotmail.com>
2019-11-17 11:21:18 +0100

    NoteDetailsViewModelFactory

--- app/src/main/java/com/halabware/didyounote/viewmodelfactories/NoteDetailsViewModelFactory.kt
+++ app/src/main/java/com/halabware/didyounote/viewmodelfactories/NoteDetailsViewModelFactory.kt
@@ -1,4 +1,19 @@
 package com.halabware.didyounote.viewmodelfactories
 
-class NoteDetailsViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.halabware.didyounote.database.NoteDao
+import com.halabware.didyounote.viewmodels.NoteDetailsViewModel
+import com.halabware.didyounote.viewmodels.NotesViewModel
+import java.lang.IllegalArgumentException
+
+class NoteDetailsViewModelFactory (private val dataSource: NoteDao) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NoteDetailsViewModel::class.java)) {
+            return NoteDetailsViewModel(dataSource = dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+
 }
